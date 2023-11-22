4b6023e2af18077cbbd00c84049fca414cea1daa
Osama Ibrahim <108794970+OsamaHIma@users.noreply.github.com>
2023-11-22 12:40:58 +0200

    dist file updated

--- dist/LanguageContext.js
+++ dist/LanguageContext.js
@@ -3,7 +3,7 @@ export var LanguageContext = createContext({
     selectedLanguage: { code: "", name: "" },
     handleChangeLanguage: function () { },
     languages: [{ code: "", name: "" }],
-    defaultLanguage: { code: "", name: "" },
+    defaultLanguage: { code: "", name: "" }
 });
 export var useLanguage = function () {
     var context = useContext(LanguageContext);
@@ -13,17 +13,17 @@ export var useLanguage = function () {
     return context;
 };
 var defaultLanguages = [
-    { code: "ar", name: "Arabic" },
+    { code: "ar", name: "العربية" },
     { code: "en", name: "English" },
-    { code: "fr", name: "French" },
-    { code: "es", name: "Spanish" },
-    { code: "de", name: "German" },
-    { code: "hi", name: "Hindi" },
-    { code: "it", name: "Italian" },
-    { code: "ja", name: "Japanese" },
-    { code: "ko", name: "Korean" },
-    { code: "zh-CN", name: "Chinese Simplified" },
-    { code: "zh-TW", name: "Chinese Traditional" },
+    { code: "fr", name: "Français" },
+    { code: "es", name: "Español" },
+    { code: "de", name: "Deutsch" },
+    { code: "hi", name: "हिन्दी" },
+    { code: "it", name: "Italiano" },
+    { code: "ja", name: "日本語" },
+    { code: "ko", name: "한국어" },
+    { code: "zh-CN", name: "中文（简体）" },
+    { code: "zh-TW", name: "中文（繁體）" },
 ];
 export var LanguageProvider = function (_a) {
     var children = _a.children, _b = _a.languages, languages = _b === void 0 ? defaultLanguages : _b, _c = _a.defaultLanguage, defaultLanguage = _c === void 0 ? { code: "en", name: "English" } : _c;
@@ -64,7 +64,7 @@ export var LanguageProvider = function (_a) {
             selectedLanguage: selectedLanguage,
             handleChangeLanguage: handleChangeLanguage,
             languages: languages,
-            defaultLanguage: defaultLanguage,
+            defaultLanguage: defaultLanguage
         };
     }, [selectedLanguage, handleChangeLanguage, languages, defaultLanguage]);
     return (React.createElement(LanguageContext.Provider, { value: value }, children));
--- dist/Translate.js
+++ dist/Translate.js
@@ -8,7 +8,7 @@ export var Translate = function (_a) {
     var _d = useState(""), translatedText = _d[0], setTranslatedText = _d[1];
     useEffect(function () {
         var translateText = function () { return __awaiter(void 0, void 0, void 0, function () {
-            var storageKey, storedText, translation, error_1, response, json, translatedText_1, fallbackError_1;
+            var storageKey, storedText, response, json, translatedText_1, fallbackError_1;
             return __generator(this, function (_a) {
                 switch (_a.label) {
                     case 0:
@@ -28,79 +28,57 @@ export var Translate = function (_a) {
                         }
                         _a.label = 1;
                     case 1:
-                        _a.trys.push([1, 3, , 9]);
-                        return [4 /*yield*/, translateWithChatGPT(children, selectedLanguage.code)];
-                    case 2:
-                        translation = _a.sent();
-                        setTranslatedText(translation);
-                        console.log("translation done by chatgpt");
-                        localStorage.setItem(storageKey, translation);
-                        return [3 /*break*/, 9];
-                    case 3:
-                        error_1 = _a.sent();
-                        console.error("ChatGPT translation failed:", error_1);
-                        _a.label = 4;
-                    case 4:
-                        _a.trys.push([4, 7, , 8]);
+                        _a.trys.push([1, 4, , 5]);
                         return [4 /*yield*/, fetch("https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=".concat(selectedLanguage.code, "&dt=t&q=").concat(children))];
-                    case 5:
+                    case 2:
                         response = _a.sent();
                         return [4 /*yield*/, response.json()];
-                    case 6:
+                    case 3:
                         json = _a.sent();
                         translatedText_1 = json[0][0][0];
                         setTranslatedText(translatedText_1);
                         localStorage.setItem(storageKey, translatedText_1);
-                        return [3 /*break*/, 8];
-                    case 7:
+                        return [3 /*break*/, 5];
+                    case 4:
                         fallbackError_1 = _a.sent();
                         console.error("Google Translate fallback failed:", fallbackError_1);
                         setTranslatedText(children);
-                        return [3 /*break*/, 8];
-                    case 8: return [3 /*break*/, 9];
-                    case 9: return [2 /*return*/];
+                        return [3 /*break*/, 5];
+                    case 5: return [2 /*return*/];
                 }
             });
         }); };
         translateText();
     }, [children, selectedLanguage, translations]);
     // Helper function to translate using ChatGPT
-    var translateWithChatGPT = function (text, targetLanguage) { return __awaiter(void 0, void 0, void 0, function () {
-        var response, data, translation, error_2;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 3, , 4]);
-                    return [4 /*yield*/, fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
-                            method: "POST",
-                            headers: {
-                                "Content-Type": "application/json",
-                                Authorization: "Bearer ".concat(process.env.OPENAI_API_KEY),
-                            },
-                            body: JSON.stringify({
-                                prompt: "Translate the following text to ".concat(targetLanguage, ": \"").concat(text, "\""),
-                                max_tokens: 100,
-                                temperature: 0.5,
-                                top_p: 1.0,
-                                n: 1,
-                                stop: "\n",
-                            }),
-                        })];
-                case 1:
-                    response = _a.sent();
-                    return [4 /*yield*/, response.json()];
-                case 2:
-                    data = _a.sent();
-                    translation = data.choices[0].text.trim();
-                    return [2 /*return*/, translation];
-                case 3:
-                    error_2 = _a.sent();
-                    console.warn("ChatGPT translation failed");
-                    return [3 /*break*/, 4];
-                case 4: return [2 /*return*/];
-            }
-        });
-    }); };
+    // const translateWithChatGPT = async (text: string, targetLanguage: string) => {
+    //   try {
+    //     // Make an API call to ChatGPT for translation
+    //     const response = await fetch(
+    //       "https://api.openai.com/v1/engines/davinci-codex/completions",
+    //       {
+    //         method: "POST",
+    //         headers: {
+    //           "Content-Type": "application/json",
+    //           Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    //         },
+    //         body: JSON.stringify({
+    //           prompt: `Translate the following text to ${targetLanguage}: "${text}"`,
+    //           max_tokens: 100,
+    //           temperature: 0.5,
+    //           top_p: 1.0,
+    //           n: 1,
+    //           stop: "\n",
+    //         }),
+    //       }
+    //     );
+    //     const data = await response.json();
+    //     const translation = data.choices[0].text.trim();
+    //     return translation;
+    //   } catch (error) {
+    //     console.warn("ChatGPT translation failed");
+    //   }
+    // };
     return React.createElement(React.Fragment, null, translatedText.toString() || children);
 };
 //# sourceMappingURL=Translate.js.map
